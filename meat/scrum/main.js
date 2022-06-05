171767468dece68e61e26e81d3a1600b348e2a86
LeraDolgova109 <Dol0914@yandex.ru>
2022-06-05 15:41:40 +0700

    многопользовательский режим

--- meat/scrum/main.js
+++ meat/scrum/main.js
@@ -31,11 +31,16 @@ const EVENTS = {
 
 class Main extends Scrum {
     commands;
-    word = null;
-    cards = new Set();
-    usedCards = new Set();
-    score = 0;
-    countRounds = 0;
+    // word = null;
+    // cards = new Set();
+    // usedCards = new Set();
+    // score = 0;
+    // countRounds = 0;
+    word = {};
+    cards ={};
+    usedCards = {};
+    score = {};
+    countRounds = {};
 
     constructor(props) {
         super(props);
@@ -51,19 +56,26 @@ class Main extends Scrum {
     }
 
     async start(ctx){
+        const peer = ctx.message.from_id
         await this.write(ctx.message.from_id, {message: `Привет! Сыйграйте 10 раундов, чтобы узнать, кто вы!!! `+
-        `Чтобы ввести ответ воспользуйтесь командой "Картинка" `+
-        `Если вам надоест, пишите "Закончить"`})
-        this.RoundInGame(ctx);
+                `Чтобы ввести ответ воспользуйтесь командой "Картинка" `+
+                `Если вам надоест, пишите "Закончить"`})
+        this.word[peer] = null;
+        this.cards[peer] = new Set();
+        this.usedCards[peer] = new Set();
+        this.score[peer] = 0;
+        this.countRounds[peer] = 0;
+        await this.RoundInGame(ctx);
     }
 
     async RoundInGame(ctx){
-        let cards = new Set(); 
+        const peer = ctx.message.from_id
+        let cards = new Set();
         while (cards.size<5){
             var newCard = getRandomInt(1,98)
-            if (!this.usedCards.has(newCard)){
+            if (!this.usedCards[peer].has(newCard)){
                 cards.add(newCard);
-                this.usedCards.add(newCard);
+                this.usedCards[peer].add(newCard);
             }
         }
         let i = 0;
@@ -89,88 +101,87 @@ class Main extends Scrum {
                 duplicate[word] = 1;
             } else {
                 ++duplicate[word]
-            } 
+            }
         }
         var uniqueWords = Object.entries(duplicate).filter(
             ([word, count]) => count === 1
         ).map(([word]) => word);
         const word = uniqueWords[getRandomInt(0, uniqueWords.length-1)]
-        this.word = word;
-        this.cards = cards;
+        this.word[peer] = word;
+        this.cards[peer] = cards;
         await this.write(ctx.message.from_id, {attachment, message: "Ваши карточки"})
         await this.write(ctx.message.from_id, {message: `Угадайте картинку по слову ${word}`})
     }
 
-    async word7(ctx, {word}){
+    async word7(ctx, { word } ){
+        const peer = ctx.message.from_id
         const picNumber = +word;
-        if(this.cards.has(picNumber)) {
-            if(this.word && this.cards.size) {
+        if(this.cards[peer].has(picNumber)) {
+            if(this.word[peer] && this.cards[peer].size) {
                 const scheme = Object.entries(WORDS);
                 for(const item of scheme) {
                     const [number, keywords] = item;
                     if (keywords.includes(this.word) && +number === picNumber) {
-                        this.word = null;
-                        this.cards = new Set();
-                        this.score += 3;
+                        this.word[peer] = null;
+                        this.cards[peer] = new Set();
+                        this.score[peer] += 3;
                         await ctx.reply('+3')
-                        this.countRounds++;
-                        if(this.countRounds < 10){
+                        ++this.countRounds[peer];
+                        if(this.countRounds[peer] < 10){
                             return this.RoundInGame(ctx);
-                        }
-                        else {
+                        } else {
                             return this.EndOfGame(ctx);
                         }
                     }
                 }
-                
+
             }
         }
-        this.word = null;
-        this.cards = new Set();
+        this.word[peer] = null;
+        this.cards[peer] = new Set();
         await ctx.reply('0')
-        this.countRounds++;
-        if(this.countRounds < 10){
+        ++this.countRounds[peer];
+        if(this.countRounds[peer] < 10){
             return this.RoundInGame(ctx);
-        }
-        else{
+        } else {
             return this.EndOfGame(ctx);
         }
     }
 
     async EndOfGame (ctx){
-        if(this.score === 30){
+        const peer = ctx.message.from_id
+        if(this.score[peer] === 30){
             await ctx.reply('Вы большой кот!');
-            await this.write(ctx.message.from_id, {message: `Ваш счет ${this.score}`});
-            this.score = 0;
-            this.countRounds = 0;
-            this.usedCards = new Set();
+            await this.write(ctx.message.from_id, {message: `Ваш счет ${this.score[peer]}`});
+            this.score[peer] = 0;
+            this.countRounds[peer] = 0;
+            this.usedCards[peer] = new Set();
         }
-        else if(this.score >= 24){
+        else if(this.score[peer] >= 24){
             await ctx.reply('Вы кот поменьше');
-            await this.write(ctx.message.from_id, {message: `Ваш счет ${this.score}`});
-            this.score = 0;
-            this.countRounds = 0;
+            await this.write(ctx.message.from_id, {message: `Ваш счет ${this.score[peer]}`});
+            this.score[peer] = 0;
+            this.countRounds[peer] = 0;
         }
-        else if(this.score >= 15){
+        else if(this.score[peer] >= 15){
             await ctx.reply('Вы маленький кот, жесть');
-            await this.write(ctx.message.from_id, {message: `Ваш счет ${this.score}`});
-            this.score = 0;
-            this.countRounds = 0;
-            this.usedCards = new Set();
+            await this.write(ctx.message.from_id, {message: `Ваш счет ${this.score[peer]}`});
+            this.score[peer] = 0;
+            this.countRounds[peer] = 0;
+            this.usedCards[peer] = new Set();
         }
-        else if(this.score >= 6){
+        else if(this.score[peer] >= 6){
             await ctx.reply('Вы няшка');
-            await this.write(ctx.message.from_id, {message: `Ваш счет ${this.score}`});
-            this.score = 0;
-            this.countRounds = 0;
-            this.usedCards = new Set();
-        }
-        else{
+            await this.write(ctx.message.from_id, {message: `Ваш счет ${this.score[peer]}`});
+            this.score[peer] = 0;
+            this.countRounds[peer] = 0;
+            this.usedCards[peer] = new Set();
+        } else {
             await ctx.reply('Ооо... победителевое');
-            await this.write(ctx.message.from_id, {message: `Ваш счет ${this.score}`});
-            this.score = 0;
-            this.countRounds = 0;
-            this.usedCards = new Set();
+            await this.write(ctx.message.from_id, {message: `Ваш счет ${this.score[peer]}`});
+            this.score[peer] = 0;
+            this.countRounds[peer] = 0;
+            this.usedCards[peer] = new Set();
         }
     }
 }
